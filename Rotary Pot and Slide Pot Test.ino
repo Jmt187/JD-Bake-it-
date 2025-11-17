a14e462199f72cc4edd53563ecd5992a4b7b8ac6
Jmt187 <156631293+Jmt187@users.noreply.github.com>
2025-11-17 15:09:03 -0500

    Update Rotary Pot and Slide Pot Test.ino

--- Rotary Pot and Slide Pot Test.ino
+++ Rotary Pot and Slide Pot Test.ino
@@ -1,25 +1,34 @@
 int potPin = 3;
-int led = 8;
+int led1 = 8;
+int led2 = 7;
 int slidePin = 4;
-
-void setup()
-{
-    pinMode(led, OUTPUT);
-    pinMode(potPin, INPUT);
-    pinMode(slidePin, INPUT);
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(potPin, INPUT);
+  pinMode(slidePin, INPUT);
+  pinMode(led2, OUTPUT);
 }
 
-void loop()
-{
-    int score = 0;
-    int potValue = analogRead(potPin);
-    int slideValue = analogRead(slidePin);
+void loop() {
+  int score = 0;
+  int potValue = analogRead(potPin);
+  int slideValue = analogRead(slidePin);
 
-    if(potValue > 300)
-    {
-        score = score + 1;
-        digitalWrite(led, HIGH);
-        delay(100);
-        digitalWrite(led, LOW);
-    }
+  if(potValue > 300)
+ {
+  score = score + 1;
+  digitalWrite(led1, HIGH);
+  delay(100);
+  digitalWrite(led1, LOW);
+ }
+
+  if(slideValue > 200)
+ {
+  score = score + 1;
+  digitalWrite(led2, HIGH);
+  delay(100);
+  digitalWrite(led2, LOW);
+ }
+
+  
 }
